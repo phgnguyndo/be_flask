8b2a8db26e808e6b6e9aa1d7a470d8e95433f330
phuong <nguyenphuongdo22@gmail.com>
2025-05-24 15:06:04 +0700

    config cors

--- src/flask_api/app.py
+++ src/flask_api/app.py
@@ -1,12 +1,15 @@
 from flask import Flask, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from config.settings import Config
+from flask_cors import CORS
 
 db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
+
+    CORS(app)
 
     db.init_app(app)
 
